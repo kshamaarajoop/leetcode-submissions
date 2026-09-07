34ab4bcd53b07e47e96faceca704eb1be10f6651
Kshamaa Rajoop <140717225+kshamaarajoop@users.noreply.github.com>
2026-09-07 13:08:40 +0530

    Optimize containsDuplicate using a HashSet
    Refactor containsDuplicate method to use a set for better performance.

--- easy/217.Duplicaes.py
+++ easy/217.Duplicaes.py
@@ -1,3 +1,13 @@
+#HashSet
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashMap = set()
+        for i in range(len(nums)):
+            if nums[i] in hashMap:
+                return True
+            hashMap.add(nums[i])
+        return False
+#HashMap
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashMap = {}
